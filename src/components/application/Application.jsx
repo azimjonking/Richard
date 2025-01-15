ad80d2a27ba7fb90fa8686528dccf86f5f42c10b
abdulatif2010 <abdulatiftorajanov2010@gmail.com>
2025-01-15 16:22:31 +0500

    application, testimonial

--- src/components/application/Application.jsx
+++ src/components/application/Application.jsx
@@ -1,6 +1,19 @@
 import "./Application.css";
+import Applicationimg from "../../assets/application/IMAGE (2).png"
 const Application = () => {
-  return <div>Application</div>;
+  return (
+    <div className="application">
+      <img className="apimg" src={Applicationimg} alt="#" />
+
+      <div className="ready">
+        <h1>Ready to get started?</h1>
+        <div className="buttons">
+        <button className="download">Download Now</button>
+        <button className="get">Get in Touch</button>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default Application;
--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,7 +1,21 @@
 import "./Testimonial.css";
-
+import Testimg from ".././../assets/testimonial/IMAGE.png"
+import Testiimg from "../../assets/testimonial/IMAGE (1).png"
 const Testimonial = () => {
-  return <div>Testimonial</div>;
+  return (
+    <div className="testimonial">
+    <img src={Testimg} alt="#" />
+    <p className="lorem">HeadNulla vitae elit libero, a pharetra augue. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Pus sit amet fermentum. Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.ing</p>
+    <div className="joshua">
+
+      <p className="josh">Joshua Home</p>
+      <p className="york">New York Times</p>
+    </div>
+
+    <img className="sapport" src={Testiimg} alt="" />
+    </div>
+    
+  )
 };
 
 export default Testimonial;
