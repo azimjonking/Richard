23c8686fd061ec8d109a94359c91a9bd09fe55bb
ibrohimo1o <ibroxim7070@icloud.com>
2025-01-15 15:46:35 +0500

    contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,13 @@
 import "./Contact.css";
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className='contact-container'>
+      <h2>Get updates from Richard</h2>
+      <p>New course alerts, discounts and free lessons</p>
+      <input type="email" placeholder='Enter your email' />
+      <span>Subscribe</span>
+    </section>
+  )
 };
 
 export default Contact;
